656a586d22ce36dcff366c8aa399f48ceefd5c46
Lincoln Bergeson <lincoln.bergeson@gmail.com>
2017-02-02 18:33:03 -0700

    Empty GUI
    Created an empty GUI for the user to enter value into a 2x2 matrix.
    Added a menu bar with no functionality
    QTableWidget auto-sizes on application load but not during in-app resize

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,4 @@
+#include <QMessageBox>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -6,6 +7,22 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // set up our table widget
+    int width = ui->matrix->verticalHeader()->width() + 4;
+
+    for (int i = 0; i < ui->matrix->columnCount(); i++) {
+        width += ui->matrix->columnWidth(i);
+    }
+    int height = ui->matrix->horizontalHeader()->height();
+    for (int i = 0; i < ui->matrix->rowCount(); i++) {
+        height += ui->matrix->rowHeight(i);
+    }
+    QSize matrixSize = QSize(width, height);
+    ui->matrix->setMaximumSize(matrixSize);
+    ui->matrix->setMinimumSize(matrixSize);
+
+
 }
 
 MainWindow::~MainWindow()
